a0f6239aaa56f523d2bc1dc1f1e27e629debfe88
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2024-07-18 16:27:13 -0300

    feat: aula-10

--- importandoexcel.py
+++ importandoexcel.py
@@ -1,10 +1,19 @@
 import pandas as pd
-excel = pd.read_excel("aula.xlsx")
+excel = pd.read_excel("vendedor.xlsx")
 
-#variável mê para controlar o fluxo de venda dos meses
-mes = input("Qual mês vc quer analisar?: ")
+# Variável 'mês' para controlar o fluxo de venda dos meses
+mes = input("Qual mês você quer analisar? ")
 
-#retornar a analisa do que mais vendou no mês 
-#ordena o mês
+# Retornar análise do vendedor do mês
+# Ordena o mês
 analise = excel.sort_values(by=f'{mes}',ascending=False)
-print(analise)
+# Melhorando...
+print(f"O melhor vendedor do mês {mes}, foi a {analise.iloc[0][0]}")
+
+# Quem menos vendeu
+analise = excel.sort_values(by=f'{mes}',ascending=True)
+# ...
+print(f"O pior vendedor do mês {mes}, foi a {analise.iloc[0][0]}")
+
+# Só quero o vendedor, como ele está na primeira coluna: [0][0]
+print(analise.iloc[0][0])
